72d6386e55daa2f16d0128df90f75824af3ae540
VRZ/AstralArchivists <rogier.van.zoelen@gmail.com>
2023-10-05 21:33:22 +0200

    Blinkies!!!!!!!!!!!!!!

--- javascript/Webamp.js
+++ javascript/Webamp.js
@@ -1,13 +1,16 @@
 new Webamp({
     initialTracks: [
-        {
-            metaData: {artist: "Izar", title: "Born of a Star",},
-            url: "/Audio/Born of a Star - Izar.m4a",
-        }, 
-        {
-            metaData: {artist: "Jammin' Sam Miller", title: "Donkey Kong Country - Aquatic Ambience [Restored]",},
-            url: "/Audio/Aquatic Ambience.mp3",
-        }, 
+        {metaData: {artist: "Izar", title: "Born of a Star",},
+        url: "/Audio/Born of a Star - Izar.m4a",},
+
+        {metaData: {artist: "Jammin' Sam Miller", title: "Donkey Kong Country - Aquatic Ambience [Restored]",},
+        url: "/Audio/Aquatic Ambience.mp3",},
+
+        {metaData: {artist: "jackie extreme",title: "bizarre love triangle",},
+        url: "https://files.catbox.moe/6eg3j3.mp3",},
+
+        {metaData: {artist: "machine girl",title: "glass ocean",},
+        url: "https://files.catbox.moe/n2qbnf.mp3",},
     ],
     initialSkin: {url: "/Webamp/spaceskin.wsz"},
 }).renderWhenReady(app);
