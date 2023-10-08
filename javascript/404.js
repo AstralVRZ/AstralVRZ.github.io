90ec8f053a02e14bc27e79f1d32bd03525cc10a3
VRZ/AstralArchivists <rogier.van.zoelen@gmail.com>
2023-10-08 18:24:02 +0200

    404page update

---
+++ javascript/404.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var path = window.location.pathname;
+    var uppercasePath = path.toUpperCase();
+    document.getElementById("path").textContent = uppercasePath;
+    });
+document.addEventListener("DOMContentLoaded", function () {
+    var path = window.location.pathname;
+    var uppercasePath = path.toUpperCase();
+    document.getElementById("path2").textContent = uppercasePath;
+    }); 
