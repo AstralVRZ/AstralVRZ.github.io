cfd1561555da3c9614f29d0747cdea851f39a31e
VRZ/AstralArchivists <rogier.van.zoelen@gmail.com>
2023-11-03 20:10:06 +0100

    Hide webamp when mobile useragent

--- javascript/Webamp.js
+++ javascript/Webamp.js
@@ -1,25 +1,29 @@
-new Webamp({
-    initialTracks: [
-        {metaData: {artist: "Izar", title: "Born of a Star",},
-        url: "/Audio/Born of a Star - Izar.m4a",},
-
-        {metaData: {artist: "Jammin' Sam Miller", title: "Donkey Kong Country - Aquatic Ambience [Restored]",},
-        url: "/Audio/Aquatic Ambience.mp3",},
-
-        {metaData: {artist: "jackie extreme",title: "bizarre love triangle",},
-        url: "https://files.catbox.moe/6eg3j3.mp3",},
-
-        {metaData: {artist: "machine girl",title: "glass ocean",},
-        url: "https://files.catbox.moe/n2qbnf.mp3",},
-
-        {metaData: {artist: "knylon/candlelitsmiles",title: "Pride (2023 Version)",},
-        url: "https://files.catbox.moe/1sbxgt.mp3",},
-        
-        {metaData: {artist: "Animadrop",title: "Into the noise",},
-        url: "/Audio/Into the noise.m4a",},
-
-        {metaData: {artist: "On Principle",title: "909girl",},
-        url: "/Audio/909Girl.mp3",},
-    ],
-    initialSkin: {url: "/Webamp/spaceskin.wsz"},
-}).renderWhenReady(app);
+if (/Android|iPhone|iPad|Opera Mini/i.test(navigator.userAgent)) {
+    window.alert("You are on mobile so I hid the webamp :3")
+} else {
+    new Webamp({
+        initialTracks: [
+            {metaData: {artist: "Izar", title: "Born of a Star",},
+            url: "/Audio/Born of a Star - Izar.m4a",},
+    
+            {metaData: {artist: "Jammin' Sam Miller", title: "Donkey Kong Country - Aquatic Ambience [Restored]",},
+            url: "/Audio/Aquatic Ambience.mp3",},
+    
+            {metaData: {artist: "jackie extreme",title: "bizarre love triangle",},
+            url: "https://files.catbox.moe/6eg3j3.mp3",},
+    
+            {metaData: {artist: "machine girl",title: "glass ocean",},
+            url: "https://files.catbox.moe/n2qbnf.mp3",},
+    
+            {metaData: {artist: "knylon/candlelitsmiles",title: "Pride (2023 Version)",},
+            url: "https://files.catbox.moe/1sbxgt.mp3",},
+            
+            {metaData: {artist: "Animadrop",title: "Into the noise",},
+            url: "/Audio/Into the noise.m4a",},
+    
+            {metaData: {artist: "On Principle",title: "909girl",},
+            url: "/Audio/909Girl.mp3",},
+        ],
+        initialSkin: {url: "/Webamp/spaceskin.wsz"},
+    }).renderWhenReady(app);
+}
