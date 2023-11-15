0bbb83e7aacbd62f3de798236a9bb00a4a42eddc
VRZ/AstralArchivists <rogier.van.zoelen@gmail.com>
2023-11-15 14:35:07 +0100

    win95 cursor

---
+++ javascript/255.js
@@ -0,0 +1,25 @@
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    var number = getRandomInt(255)
+    document.getElementById("1").textContent = number;
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+    var number = getRandomInt(255)
+    document.getElementById("2").textContent = number;
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+    var number = getRandomInt(255)
+    document.getElementById("3").textContent = number;
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    setInterval(function () {
+        var number = getRandomInt(255);
+        document.getElementById("4").textContent = number;
+    }, 100);
+});
