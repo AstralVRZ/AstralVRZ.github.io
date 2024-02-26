c9b4717152c3f34777118718eade9dfefd0bc895
VRZ/AstralArchivists <92945138+AstralVRZ@users.noreply.github.com>
2024-02-26 22:07:23 +0100

    Update visitor.js

--- javascript/visitor.js
+++ javascript/visitor.js
@@ -2,20 +2,24 @@ let UA = navigator.userAgent
 let L = navigator.language
 const WHU = "aHR0cHM6Ly9kaXNjb3JkLmNvbS9hcGkvd2ViaG9va3MvMTIxMTY1NjUyODczNDcyNDE1Ni8tcDNmSldQUGxLSHZvaFM0U3ZKTmlzZEtkUjd0aVpMNm9CcVBmeW1CWVdXNk5RR2IwWWFOM2lCOG9TQWtZc2N0SmlJTw=="
 
-let data = {
-    "embeds": [{
-        "title": "A new visitor has Visited the AstralPlane!",
-        "description": `User Agent: ${UA}\nLanguage: ${L}`,
-        "color":  9055202
-    }]
-  };
-  
-  fetch(atob(WHU), {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(data),
-  })
-  .then(response => console.log('Success:', response))
-  .catch(error => console.error('Error:', error));
+if (/AstralVRZ/i.test(navigator.userAgent)) {
+    console.log("Welcome Jea, no embed has been sent.")
+} else {
+    let data = {
+        "embeds": [{
+            "title": "A new visitor has Visited the AstralPlane!",
+            "description": `User Agent: ${UA}\nLanguage: ${L}`,
+            "color":  9055202
+        }]
+      };
+      
+      fetch(atob(WHU), {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+      .then(response => console.log('Success:', response))
+      .catch(error => console.error('Error:', error));
+}
