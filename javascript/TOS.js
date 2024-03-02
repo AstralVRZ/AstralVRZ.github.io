965c90f81787e68aa54ba187a92dfee7a7782b87
VRZ/AstralArchivists <92945138+AstralVRZ@users.noreply.github.com>
2024-03-02 16:57:50 +0100

    for the lolz

---
+++ javascript/TOS.js
@@ -0,0 +1,14 @@
+document.getElementById('accept').addEventListener("click", function() {
+    localStorage.setItem("TOS", "True");
+    const element = document.querySelector('.disclaimer');
+    element.style.display = 'none';
+    const title = document.querySelector('#title');
+    title.style.top = '3%';
+})
+var TOS = localStorage.getItem("TOS")
+if (TOS == "True") {
+    const element = document.querySelector('.disclaimer');
+    element.style.display = 'none';
+    const title = document.querySelector('#title');
+    title.style.top = '3%';
+}
