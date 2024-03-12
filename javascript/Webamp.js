845d59c15ed7da5919b7c2b25972548dbdd38971
VRZ/AstralArchivists <92945138+AstralVRZ@users.noreply.github.com>
2024-03-13 00:08:56 +0100

    move to local

--- javascript/Webamp.js
+++ javascript/Webamp.js
@@ -4,22 +4,22 @@ if (/Android|iPhone|iPad|Opera Mini/i.test(navigator.userAgent)) {
     new Webamp({
         initialTracks: [
             {metaData: {artist: "Izar", title: "Born of a Star",},
-            url: "/Audio/Born of a Star - Izar.m4a",},
+            url: "/Audio/BornOfaStar-Izar.m4a",},
     
             {metaData: {artist: "Jammin' Sam Miller", title: "Donkey Kong Country - Aquatic Ambience [Restored]",},
-            url: "/Audio/Aquatic Ambience.mp3",},
+            url: "/Audio/AquaticAmbience.mp3",},
     
-            {metaData: {artist: "jackie extreme",title: "bizarre love triangle",},
-            url: "https://files.catbox.moe/6eg3j3.mp3",},
+            {metaData: {artist: "jackie extreme",title: "Bizarre Love Triangle",},
+            url: "/Audio/BizarreLoveTriangle.mp3",},
     
-            {metaData: {artist: "machine girl",title: "glass ocean",},
-            url: "https://files.catbox.moe/n2qbnf.mp3",},
+            {metaData: {artist: "machine girl",title: "Glass Ocean",},
+            url: "/Audio/GlassOcean.mp3",},
     
             {metaData: {artist: "knylon/candlelitsmiles",title: "Pride (2023 Version)",},
-            url: "https://files.catbox.moe/1sbxgt.mp3",},
+            url: "/Audio/Pride2023.mp3",},
             
             {metaData: {artist: "Animadrop",title: "Into the noise",},
-            url: "/Audio/Into the noise.m4a",},
+            url: "/Audio/IntoTheNoise.m4a",},
     
             {metaData: {artist: "On Principle",title: "909girl",},
             url: "/Audio/909Girl.mp3",},
