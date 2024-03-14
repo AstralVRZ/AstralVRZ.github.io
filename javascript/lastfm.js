4951509cd29140fd307e704fa48c7ab3c2af5f3a
VRZ/AstralArchivists <92945138+AstralVRZ@users.noreply.github.com>
2024-03-14 21:04:57 +0100

    Lastfm + fonts
    - Made a little last.fm widget
    - converted fonts to otf
    - changing around the divs

---
+++ javascript/lastfm.js
@@ -0,0 +1,18 @@
+const K = "cfc71a327cd84200077e595e96245eaf"
+const U = "AstralArchivist"
+
+const getInfo = async () => {
+    const request = await fetch(`http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${U}&api_key=${K}&limit=1&format=json`)
+    const json = await request.json();
+    // console.log(json)
+    // console.log(request)
+    document.getElementById('songName').innerHTML = json.recenttracks.track[0].name;i
+    document.getElementById('artistName').innerHTML = json.recenttracks.track[0].artist["#text"];
+    document.getElementById('songLink').href = json.recenttracks.track[0].url
+    if (json.recenttracks.track[0].image[2]["#text"] == "") {
+        document.getElementById('songImage').src = "/Images/misc/player_default_album.png"
+    } else {
+        document.getElementById('songImage').src = json.recenttracks.track[0].image[3]["#text"];
+    }
+}
+getInfo();
