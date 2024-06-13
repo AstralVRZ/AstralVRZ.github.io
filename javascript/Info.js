49d12c7534c584d859e37c5429107cada5563ec7
VRZ/AstralArchivists <92945138+AstralVRZ@users.noreply.github.com>
2024-06-13 20:54:13 +0200

    info, privacy, css fix

---
+++ javascript/Info.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const info = document.getElementById('info')
+    const lastMod = document.lastModified
+    info.innerHTML =  lastMod
+})
