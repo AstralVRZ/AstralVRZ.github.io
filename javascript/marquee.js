1e99caf2d30e9749c71e9bbb7156d59134459abd
VRZ/AstralArchivists <92945138+AstralVRZ@users.noreply.github.com>
2024-06-20 20:39:04 +0200

    multi marquee fix

--- javascript/marquee.js
+++ javascript/marquee.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
-    const marqueeContent = document.querySelector('.marquee-content');
+    const marqueeContent = document.querySelectorAll('.marquee-content');
+    marqueeContent.forEach(marqueeContent => {
         marqueeContent.addEventListener('mouseover', () => marqueeContent.style.animationPlayState = 'paused');
         marqueeContent.addEventListener('mouseout', () => marqueeContent.style.animationPlayState = 'running');
+    });
 })
