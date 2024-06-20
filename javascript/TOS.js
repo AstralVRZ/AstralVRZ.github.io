034372e6b8c1dd5f606ec5f46a12d7ae66db0cc4
VRZ/AstralArchivists <92945138+AstralVRZ@users.noreply.github.com>
2024-06-20 22:26:11 +0200

    js clean up and webamp yeet

--- javascript/TOS.js
+++ javascript/TOS.js
@@ -1,14 +1,16 @@
-document.getElementById('accept').addEventListener("click", function() {
-    localStorage.setItem("TOS", "True");
-    const element = document.querySelector('.disclaimer');
-    element.style.display = 'none';
-    const title = document.querySelector('#title');
-    title.style.top = '3%';
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('accept').addEventListener("click", function() {
+        localStorage.setItem("TOS", "True");
+        const element = document.querySelector('.disclaimer');
+        element.style.display = 'none';
+        const title = document.querySelector('#title');
+        title.style.top = '3%';
+    })
+    var TOS = localStorage.getItem("TOS")
+    if (TOS == "True") {
+        const element = document.querySelector('.disclaimer');
+        element.style.display = 'none';
+        const title = document.querySelector('#title');
+        title.style.top = '3%';
+    }
 })
-var TOS = localStorage.getItem("TOS")
-if (TOS == "True") {
-    const element = document.querySelector('.disclaimer');
-    element.style.display = 'none';
-    const title = document.querySelector('#title');
-    title.style.top = '3%';
-}
